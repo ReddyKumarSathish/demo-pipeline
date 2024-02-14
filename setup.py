41641847cf0084b25aa8315a0d571c1cce24d5ee
ReddyKumarSathish <sathishkumarreddy441@gmail.com>
2024-02-14 08:45:52 +0530

    all files added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(name="cences-income",
+      version="0.0.1",
+      author="sathish",
+      author_email="sathishkumarreddy1998@gmail.com",
+      packages=find_packages(),
+      install_requries=["pandas","numpy","flask"]
+      )
